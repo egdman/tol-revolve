2bbf6e05d27ef469ab72afc3546442749285f82e
egdman <egdman90@gmail.com>
2018-05-22 00:01:28 +0700

    fixes in compile-proto.py

--- tol/angle/compile-proto.py
+++ tol/angle/compile-proto.py
@@ -13,11 +13,12 @@ gazebo_include_dir = hit.group(1)
 gazebo_proto_dir = os.path.join(gazebo_include_dir, "gazebo", "msgs", "proto")
 
 package_dir = 'spec'
-proto_dir = 'spec/msgs'
+proto_dir = 'msgs'
+generated_dir = os.path.join(package_dir, proto_dir)
 
 # generate protobuf python files
 check_output(
-    'protoc -I {GAZEBO_INCLUDE_DIR} -I {GAZEBO_PROTO_DIR} -I spec --python_out={PKG_DIR}/ {PROTO_DIR}/*.proto'
+    'protoc -I {GAZEBO_INCLUDE_DIR} -I {GAZEBO_PROTO_DIR} -I {PKG_DIR} --python_out={PKG_DIR}/ {PKG_DIR}/{PROTO_DIR}/*.proto'
     .format(
         GAZEBO_INCLUDE_DIR = gazebo_include_dir,
         GAZEBO_PROTO_DIR = gazebo_proto_dir,
@@ -26,15 +27,18 @@ check_output(
     shell=True)
 
 
-# fix bullshit include syntax in protobuf-generated python files
-generated_files = (f for f in check_output("ls spec/msgs", shell=True).decode('utf-8').split() if fnmatch.fnmatch(f, "*_pb2.py"))
 
-# all imports not containing msgs. and ending with _pb2 are assumed to be from pygazebo.msg
-gazebo_package_finder = re.compile(r'^import\s+((?!msgs\.).*_pb2)', re.MULTILINE)
+# fix bullshit include syntax in protobuf-generated python files
+generated_files = (f for f in check_output("ls {}".format(generated_dir), shell=True)
+    .decode('utf-8').split() if fnmatch.fnmatch(f, "*_pb2.py"))
+
+# all imports not starting with ${proto_dir}. and ending with _pb2 are assumed to be from pygazebo.msg
+patt = "^import\s+((?!{PROTO_DIR}\.).*_pb2)".format(PROTO_DIR = proto_dir)
+gazebo_package_finder = re.compile(patt, re.MULTILINE)
 replace_with = r'from pygazebo.msg import \1'
 
 for fname in generated_files:
-    fpath = os.path.join(proto_dir, fname)
+    fpath = os.path.join(generated_dir, fname)
     with open(fpath, 'r') as stream:
         text = stream.read()
 
