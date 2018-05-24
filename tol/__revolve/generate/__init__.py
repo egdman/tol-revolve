b4050f749cb8701a219a3d8383c022e7ca2cf04e
egdman <egdman90@gmail.com>
2018-05-24 23:06:53 +0700

    removed local revolve directory

--- tol/__revolve/generate/__init__.py
+++
@@ -1,4 +0,0 @@
-from .body import BodyGenerator, FixedOrientationBodyGenerator
-from .generate import TreeGenerator
-from .neural_net import NeuralNetworkGenerator
-from .representation import Tree, Node
